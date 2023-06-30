9147ab504c31a108ee32be26e0fe711ea65da8ad
huaixu3 <huaixu3@qq.com>
2023-06-30 10:16:55 +0800

    feat: crypto module

---
+++ src/lib/service/cryptoImpl.js
@@ -0,0 +1,25 @@
+var CryptoJS = require('crypto-js');
+
+const encryptoKey = '--mhyt.fun------';
+//加密
+const encrypt = (data,paramkey = encryptoKey) => {
+  var key = CryptoJS.enc.Utf8.parse(paramkey);
+  var mydata = CryptoJS.enc.Utf8.parse(data);
+  var udata = CryptoJS.AES.encrypt(mydata, key, {
+    mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7, // 填充方
+  });
+  var encrypted = udata.toString();
+  return encrypted;
+};
+//解密
+const decrypt = (data,paramKey = encryptoKey) => {
+  var key = CryptoJS.enc.Utf8.parse(paramKey);
+  var udata = CryptoJS.AES.decrypt(data, key, {
+    mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7,
+  });
+  var decrypted = udata.toString(CryptoJS.enc.Utf8);
+  return decrypted;
+};
+
+module.exports = {encrypt, decrypt};
+
